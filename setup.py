ea42383ca96fd13d1cc7ee23edc5f0d7935afdb9
grro <gregor.roth@1und1.de>
2020-09-24 09:55:19 +0200

    long description removed

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 from setuptools import setup
 
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='pi_dht_webthing',
     packages=['pi_dht_webthing'],
     version="0.0.1",
     description='A web connected humidity and temperature sensor',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     license='MIT',
     author='Gregor Roth',
     author_email='gregor.roth@web.de',
